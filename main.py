9afcb569cf21eb297efe663b456cf04456a53032
Felipe Capati <feli.capati@hotmail.com>
2019-09-07 00:55:57 -0300

    workers

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 #!/usr/bin/env python
+from datetime import datetime
 from mpi4py import MPI
-import numpy
+from math import floor
+import numpy as np
 import sys
 
+# Setup MPI
+MAXPROCS = 4
 comm = MPI.COMM_SELF.Spawn(sys.executable,
                            args=['worker_monte_carlo.py'],
-                           maxprocs=4)
+                           maxprocs=MAXPROCS)
 rank = comm.Get_rank()
 
-N = numpy.array(100, 'i')
-comm.Bcast([N, MPI.INT], root=MPI.ROOT)
-PI = numpy.array(0.0, 'd')
-comm.Reduce(None, [PI, MPI.DOUBLE],
+# Configure total number of integration
+n_total = np.array(80000, 'i')
+
+# Init Monte Carlo Work
+now = datetime.now()
+
+# Configure Limit of Integration
+lim_x_min = 1
+lim_x_max = 4
+lim_y_min = -3
+lim_y_max = 4
+lim_z_min = -2
+lim_z_max = 2
+
+# Send MPI Broadcast requisition
+requesition = np.array(floor(n_total/MAXPROCS), 'i')
+comm.Bcast([requesition, MPI.INT], root=MPI.ROOT)
+
+# Get Reduce MPI
+n_fig = np.array(0.0, 'i')
+comm.Reduce(None, [n_fig, MPI.INT],
             op=MPI.SUM, root=MPI.ROOT)
-print("rank(%s): %s" %(rank, PI))
+
+# Calculate volume of figure
+v_total = (lim_x_max - lim_x_min) * (lim_y_max - lim_y_min) * (lim_z_max - lim_z_min)
+v_figura = round(v_total * (n_fig / float(requesition*MAXPROCS)), 2)
+
+# End Monte Carlo Work
+time_process = round((datetime.now() - now).total_seconds(), 2)
+
+print("> rank(%s) :: Volume: %s :: TimeToProcess: %s" %(rank, v_figura, time_process))
 
 comm.Disconnect()
+
+print("a")
--- worker_monte_carlo.py
+++ worker_monte_carlo.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
+from random import uniform
 from mpi4py import MPI
-import numpy
+import numpy as np
 
+# Setup MPI Worker
 comm = MPI.Comm.Get_parent()
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-N = numpy.array(0, dtype='i')
-comm.Bcast([N, MPI.INT], root=0)
-h = 1.0 / N; s = 0.0
-for i in range(rank, N, size):
-    x = h * (i + 0.5)
-    s += 4.0 / (1.0 + x**2)
-PI = numpy.array(s * h, dtype='d')
-comm.Reduce([PI, MPI.DOUBLE], None,
-            op=MPI.SUM, root=0)
+# Get data from root
+n_total = np.array(0, dtype='i')
+comm.Bcast([n_total, MPI.INT], root=0)
 
+# Configure Limit of Integration
+lim_x_min = 1
+lim_x_max = 4
+lim_y_min = -3
+lim_y_max = 4
+lim_z_min = -2
+lim_z_max = 2
+
+# Calculate part of Monte Carlo
+n_fig = 0
+for i in range(0, n_total):
+    x_random = uniform(lim_x_min, lim_x_max)
+    y_random = uniform(lim_y_min, lim_y_max)
+    z_random = uniform(lim_z_min, lim_z_max)
+
+    toroid = z_random ** 2 + ((x_random ** 2 + y_random ** 2) ** 0.5 - 3) ** 2
+    if (x_random > 1) and (y_random >= -3) and (toroid <= 1):
+        n_fig += 1
+
+# Export count information using Reduce by sum
+np_n_fig = np.array(n_fig, dtype='i')
+comm.Reduce([np_n_fig, MPI.INT], None, op=MPI.SUM, root=0)
+
+# Diconnect
 comm.Disconnect()
